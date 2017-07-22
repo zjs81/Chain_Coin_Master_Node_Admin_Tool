5afd79381fed88f5bd7c706275b6b91dbb5fa807
zjs81 <30362347+zjs81@users.noreply.github.com>
2017-07-22 14:54:07 -0700

    Create remote.py

---
+++ remote.py
@@ -0,0 +1,43 @@
+from appJar import gui
+keyfilename = ""
+keyfileinuse = False
+app = gui("HODLER ADMIN")
+def press(button):
+    if button == "Cancel":
+        app.stop()
+    else:
+        ip = app.getEntry("IP")
+        usr = app.getEntry("Username")
+        pwd = app.getEntry("Password")
+        port = app.getEntry("Port")
+        print("User:", usr, "Pass:", pwd)
+        app.okBox("alert", "Trying to connect to the server.")
+def keyfile(button):
+    global keyfilename
+    global keyfileinuse
+    keyfilename = app.openBox(title=None, dirName=None, fileTypes=None, asFile=False)
+    if(keyfilename == ""):
+        pass
+    else:
+        keyfileinuse = True
+    print keyfileinuse
+    print keyfilename
+app = gui("Login Window")
+
+
+app.setSticky("ew")
+app.setFont(20)
+
+app.startLabelFrame("Remote Login Details")
+app.addLabel("l1", "IP", 0, 0)
+app.addEntry("IP", 0, 1)
+app.addLabel("l2", "Port", 1, 0)
+app.addEntry("Port", 1, 1)
+app.addLabel("l4", "Username", 2, 0)
+app.addEntry("Username", 2, 1)
+app.addLabel("l3", "Password", 3, 0)
+app.addSecretEntry("Password", 3, 1)
+app.addButtons(["Submit", "Cancel"], press, 4, 0, 4)
+app.addButtons(["Add ssh key file"], keyfile, 5, 0, 5)
+app.stopLabelFrame()
+app.go()
