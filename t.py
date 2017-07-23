2c610f2c40a14bc2a3b0af6e906decc9214e500d
zjs81 <30362347+zjs81@users.noreply.github.com>
2017-07-22 18:51:40 -0700

    Update t.py

--- t.py
+++ t.py
@@ -2,6 +2,21 @@ from appJar import gui
 import sys
 import paramiko
 
+#Def Func
+def startmaster():
+    print "lol"
+def send():
+    pass
+def restart():
+    pass
+def startwallet():
+    pass
+def setup():
+    pass
+def refresh():
+    pass
+#Done Def Func
+#Starts takeing args
 keyfilename = ""
 keyfileinuse = False
 port = int(sys.argv[4])
@@ -12,26 +27,24 @@ password = sys.argv[3]
 balance = 0
 #Gets balance
 #ssh connection start
-try:
-    cmd='uptime'
-    ssh=paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(ip,port,username,password)
-    stdin,stdout,stderr=ssh.exec_command(cmd)
-    outlines=stdout.readlines()
-    resp=''.join(outlines)
-    print(resp)
+cmd='uptime'
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ip,port,username,password)
+stdin,stdout,stderr=ssh.exec_command(cmd)
+outlines=stdout.readlines()
+resp=''.join(outlines)
+print(resp)
 
 #ssh connection end
-
-app = gui("HODLER ADMIN", "400x200")
+app = gui()
 app.setFont(10)
 app.addButton("Start masternode", startmaster)
 app.addButton("Send chc from masternode", send)
 app.addButton("restart server", restart)
 app.addButton("Start masternode wallet", startwallet)
 app.addButton("open setup", setup)
-app.addButton("Refresh Balance", Refresh Balance)
-app.infoBox("Alert", "Its recomended that you start your masternode wallet first then wait about 3 minutes before sending or starting the masternode. So It can sync")
+app.addButton("Refresh Balance", refresh)
+app.infoBox("Alert", "Its recomended that you start your masternode wallet first then wait about 3 minutes before sending from or starting the masternode. So It can sync")
 
 app.go()
